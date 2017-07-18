595ac70f6c2f0a92ac2846371152f7c7c53d1215
hungrysquirrel <joshmaxrubinstein@gmail.com>
2017-07-18 10:47:46 -0700

    cleanup and comment

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,7 +1,9 @@
-import '@storybook/addon-actions/register';
+// Storybook addons
+import '@storybook/addon-actions/register'
 import '@storybook/addon-knobs/register'
 
+
 // Brand.ai integration
-import '@storybook/react/addons';
-import register from 'brandai-storybook';
-register({ dataUrl:'https://assets.brand.ai/hungrysquirrel/t-2-ui-kit/style-data.json?exportFormat=list&key=r1OLNVOQW' });
+import '@storybook/react/addons'
+import register from 'brandai-storybook'
+register({ dataUrl:'https://assets.brand.ai/hungrysquirrel/t-2-ui-kit/style-data.json?exportFormat=list&key=r1OLNVOQW' })
